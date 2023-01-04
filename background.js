9a2a33ee031950fd1f86e968ebc8044f99cf7f58
Sridhar Raj Sampath Kumar <sridhar@aghenam.com>
2023-01-04 18:12:49 +0530

    feat(chrome): add support for chrome (#2)

--- background.js
+++ background.js
@@ -1,38 +1,53 @@
-browser.commands.onCommand.addListener((command) => {
-  browser.tabs.query({})
-    .then((tabs) => {
-      // intentionally using ==, please read https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/commands/onCommand
-      // for more on this.
+const isChrome = navigator.userAgent.match(/chrome/ig);
+const isFireFox = navigator.userAgent.match(/firefox/ig);
 
-      const unPinnedTabs = tabs.filter(({ pinned }) => pinned === false);
+if (typeof browser === "undefined") {
+  browser = chrome;
+}
 
-      const currentTab = tabs.find(({ active }) => active);
-      const currentTabIndex = currentTab.index;
+const setCommands = ({ command, tabs }) => {
+  const unPinnedTabs = tabs.filter(({ pinned }) => pinned === false);
 
-      if (command == 'close-all') {
-        for (const tab of unPinnedTabs) {
-          const tabId = tab.id;
-          if (tabId !== currentTab.id) {
-            browser.tabs.remove(tabId);
-          }
-        }
-      } else if (command == 'close-right') {
-        const tabsToRight = unPinnedTabs.reduce((tabIds, { id: tabId, index }) => {
-          if (index > currentTabIndex) {
-            tabIds.push(tabId);
-          }
-          return tabIds;
-        }, []);
-        browser.tabs.remove(tabsToRight);
-      } else if (command == 'close-left') {
-        const tabsToLeft = unPinnedTabs.reduce((tabIds, { id: tabId, index }) => {
-          if (index < currentTabIndex) {
-            tabIds.push(tabId);
-          }
-          return tabIds;
-        }, []);
-        browser.tabs.remove(tabsToLeft);
+  const currentTab = tabs.find(({ active }) => active);
+  const currentTabIndex = currentTab.index;
+
+  // intentionally using ==, please read https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/commands/onCommand
+  // for more on this.
+  if (command == 'close-all') {
+    for (const tab of unPinnedTabs) {
+      const tabId = tab.id;
+      if (tabId !== currentTab.id) {
+        browser.tabs.remove(tabId);
       }
-    });
+    }
+  } else if (command == 'close-right') {
+    const tabsToRight = unPinnedTabs.reduce((tabIds, { id: tabId, index }) => {
+      if (index > currentTabIndex) {
+        tabIds.push(tabId);
+      }
+      return tabIds;
+    }, []);
+    browser.tabs.remove(tabsToRight);
+  } else if (command == 'close-left') {
+    const tabsToLeft = unPinnedTabs.reduce((tabIds, { id: tabId, index }) => {
+      if (index < currentTabIndex) {
+        tabIds.push(tabId);
+      }
+      return tabIds;
+    }, []);
+    browser.tabs.remove(tabsToLeft);
+  }
+}
 
+browser.commands.onCommand.addListener((command) => {
+  if (isChrome) {
+    browser.tabs.query({}, (tabs) => {
+      setCommands({ command, tabs });
+    });
+  } else if (isFireFox) {
+    browser.tabs.query({})
+      .then((tabs) => {
+        setCommands({ command, tabs });
+      });
+  }
 });
